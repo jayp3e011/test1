df8cbca38902ba0ec3bd4c41b257aa3f8273a4d5
jayp3e011 <jayp3e.bautista@gmail.com>
2017-03-03 01:29:41 +0800

    make it better

--- app/models/conf.php
+++ app/models/conf.php
@@ -1,14 +1,22 @@
 <?php
-	define('HOST', 'localhost');
-	define('USER', 'root');
-	define('PASS', '');
-	define('DBSE', 'test');
+	// define('HOST', 'localhost');
+	// define('USER', 'root');
+	// define('PASS', '');
+	// define('DBSE', 'test');
 
-	$link = mysqli_connect(HOST, USER, PASS, DBSE);
-	if (!$link) {
-	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	    exit;
-	}
+	// $link = mysqli_connect(HOST, USER, PASS, DBSE);
+	// if (!$link) {
+	//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	//     exit;
+	// }
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+	$link->register(new Herrera\Pdo\PdoServiceProvider(),
+	   array(
+	       'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+	       'pdo.username' => $dbopts["user"],
+	       'pdo.password' => $dbopts["pass"]
+	   )
+	);
 ?>
