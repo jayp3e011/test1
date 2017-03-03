0ff463e0240057b324f0918f8ea9e781ba23c748
jayp3e011 <jayp3e.bautista@gmail.com>
2017-03-03 13:45:46 +0800

    sql connection updated

--- app/controllers/php/dbconnect.php
+++ app/controllers/php/dbconnect.php
@@ -4,8 +4,9 @@ define('HOST', 'sql6.freemysqlhosting.net');
 define('USER', 'sql6161771');
 define('PASS', 'BzWAKbzp8a');
 define('DBSE', 'sql6161771');
+define('PORT', '3306');
 
-$link = mysqli_connect(HOST, USER, PASS, DBSE);
+$link = mysqli_connect(HOST, USER, PASS, DBSE, PORT);
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
