e148e6787307970e767afdfe03f22acf684f19aa
jayp3e011 <jayp3e.bautista@gmail.com>
2017-03-03 15:23:11 +0800

    sql connection updated

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,7 +3,7 @@ function doRenderTable(id){
         method: "POST",
         url: "../app/models/user.php"
     }).done(function(userdata){
-    	console.log(userdata);
+    	// console.log(userdata);
     	_USERTABLE_DATA = JSON.parse(userdata);
     	renderTable(id,userdata,['id', 'email', 'password', 'firstname', 'lastname', 'createat', 'isadmin'],['create','read','update','delete']);	
 	});
